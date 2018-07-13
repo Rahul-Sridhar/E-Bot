71c6ce440f61533d3a68420fda28af7d5a7973f6
Rahul-Sridhar <rahul.sridhar97@yahoo.com>
2018-07-13 16:09:14 +0100

    Comments added

--- Remote Server/flask_server.py
+++ Remote Server/flask_server.py
@@ -31,22 +31,30 @@ def hello():
 
 @app.route('/photo1', methods=["GET"])
 def get_photo():
+
+    # Creates a string and stores it in a variable. To be used later to save an image file by this string
     global date_string
     date_string=str(datetime.datetime.now())
     date_string='image'+date_string+'.jpg'
     date_string=date_string.replace(":", "")
     date_string=date_string.replace(" ", "")
-    url="http://172.17.59.97/sending_photo"
+
+    # Sends GET request to raspberry pi for an image using get() of requests library by passing url and authentication details (username and password) as parameters 
+    url="http://172.17.35.212/sending_photo"
     r = requests.get(url, auth=('pi', 'raspberry'))
+
+    # Store the image by extracting the data (using r.content) from the request 
     with open("C:\\Users\\RAHUL\\Downloads\\" + date_string, 'wb+') as f:
           f.write(r.content)
 
-    with open(r"C:\Users\RAHUL\Downloads\facial_expression_model_structure.json", "r") as f:
+    # Used model_from_json() to store the model structure from the json file
+    with open(r"C:\Users\RAHUL\Downloads\dev\HelloWorld\tensorflow-101-master\model\facial_expression_model_structure.json", "r") as f:
         model = model_from_json(f.read())
 
-    # Load weights into the new model
-    model.load_weights(r"C:\Users\RAHUL\Downloads\facial_expression_model_weights.h5")
+    # Load weights into the new model.
+    model.load_weights(r"C:\Users\RAHUL\Downloads\dev\HelloWorld\tensorflow-101-master\model\facial_expression_model_weights.h5")
 
+    # Convert image into 48*48 grayscale image
     img = image.load_img("C:\\Users\\RAHUL\\Downloads\\"+date_string, grayscale=True, target_size=(48, 48))
 
     x = image.img_to_array(img)
@@ -56,7 +64,7 @@ def get_photo():
 
     custom = model.predict(x)
 
-    #function for drawing bar chart for emotion preditions
+    # Returns the emotion based on the prediction of the deep learning model
     def emotion_analysis(emotions):
         objects = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
         max1=0
@@ -68,8 +76,11 @@ def get_photo():
         return objects[pos]
 
     s=emotion_analysis(custom[0])
-    urla="http://172.17.59.97/emotion?emotions="+s
+
+    # Stores the emotion in emotions parameter of the url and sends a GET request with the URL and authentication details (Username and Password) as parameters 
+    urla="http://172.17.35.212/emotion?emotions="+s
     r1 = requests.get(urla, auth=('pi', 'raspberry'))
+
     K.clear_session()
     return s
 
